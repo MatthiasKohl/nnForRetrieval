1d34ffd607d4e20a2256ea4c8f41eac9f456be8c
Matthias Kohl <matthias.kohl@hotmail.de>
2017-06-09 17:13:18 +0200

    added dba at instance level

--- test/siamese_descriptor_test.py
+++ test/siamese_descriptor_test.py
@@ -8,6 +8,7 @@ from model.nn_utils import set_net_train
 from utils import *
 from train.siamese_descriptor import P, labels
 from train.siamese_descriptor import get_embeddings, get_siamese_net
+from instance_avg import instance_avg
 
 
 def usage():
@@ -24,11 +25,13 @@ def usage():
           'If negative, CPU is used.\n')
     o5 = ('--feature-dim=\t<int>\tThe feature dimensionality of the network.\n')
     o6 = ('--batch=\t<int>\tThe batch size to use.\n')
-    o7 = '--help\t\tShow this help\n'
-    print(prefix + o1 + o2 + o3 + o4 + o5 + o6 + o7)
+    o7 = ('--dba=\t<int>\tUse DBA with given k. If k = 0, do not use DBA. ' +
+          'If k<0, use all neighbors within the same instance.\n')
+    o8 = '--help\t\tShow this help\n'
+    print(prefix + o1 + o2 + o3 + o4 + o5 + o6 + o7 + o8)
 
 
-def main(dataset_full, model, weights, device, feature_dim, batch_size):
+def main(dataset_full, model, weights, device, feature_dim, batch_size, dba):
     # training and test sets
     dataset_id = parse_dataset_id(dataset_full)
     match_labels = match_label_functions[dataset_id]
@@ -74,17 +77,28 @@ def main(dataset_full, model, weights, device, feature_dim, batch_size):
     sim = torch.mm(test_embeddings, ref_embeddings.t())
     prec1, c, t, _, _ = precision1(sim, test_set, test_train_set)
     mAP = mean_avg_precision(sim, test_set, test_train_set)
-    print('Descriptor (TEST): {0} / {1} - acc: {2:.4f} - mAP:{3:.4f}\n'.format(c, t, prec1, mAP))
+    print('Descriptor (TEST): {0} / {1} - acc: {2:.4f} - mAP:{3:.4f}'.format(c, t, prec1, mAP))
+    if dba == 0:
+        return
+    print('Testing using instance feature augmentation')
+    dba_embeddings, dba_set = instance_avg(device, ref_embeddings,
+                                           test_train_set, labels, dba)
+    sim = torch.mm(test_embeddings, dba_embeddings.t())
+    prec1, c, t, _, _ = precision1(sim, test_set, dba_set)
+    mAP = mean_avg_precision(sim, test_set, dba_set)
+    print('Descriptor (TEST DBA k={4}): {0} / {1} - acc: {2:.4f} - mAP:{3:.4f}'.format(c, t, prec1, mAP, dba))
 
 
 if __name__ == '__main__':
-    options_l = (['help', 'dataset=', 'model=', 'weights=', 'device='])
+    options_l = (['help', 'dataset=', 'model=', 'weights=', 'device=',
+                 'feature-dim=', 'batch=', 'dba='])
     try:
         opts, args = getopt.getopt(sys.argv[1:], '', options_l)
     except getopt.GetoptError:
         usage()
         sys.exit(2)
-    dataset_full, model, weights, device, feature_dim, batch_size = None, None, None, None, None
+    dataset_full, model, weights, device = None, None, None, None
+    feature_dim, batch_size, dba = None, None, -1
     for opt, arg in opts:
         if opt in ('--help'):
             usage()
@@ -101,6 +115,8 @@ if __name__ == '__main__':
             feature_dim = check_int(arg, 'feature-dim', usage)
         elif opt in ('--batch'):
             batch_size = check_int(arg, 'batch', usage)
+        elif opt in ('--dba'):
+            dba = check_int(arg, 'dba', usage)
     if (dataset_full is None or model is None or
             weights is None or device is None or
             feature_dim is None or batch_size is None):
@@ -110,7 +126,8 @@ if __name__ == '__main__':
 
     with torch.cuda.device(device):
         try:
-            main(dataset_full, model, weights, device, feature_dim, batch_size)
+            main(dataset_full, model, weights, device, feature_dim,
+                 batch_size, dba)
         except:
             log_detail(P, None, traceback.format_exc())
             raise
